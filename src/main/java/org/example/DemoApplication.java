7c509f3bed896bd5bcbbc10b785b7f9bba48eb3d
micha <mimagra@gmx.net>
2023-05-01 22:33:28 +0200

    init

---
+++ src/main/java/org/example/DemoApplication.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private Long matrikelnummer;
+
+    @Column
+    private String vorname;
+
+    @Column
+    private String nachname;
+
+
+    public Student(long matrikelnummer, String vorname, String nachname) {
+        this.matrikelnummer = matrikelnummer;
+        this.vorname = vorname;
+        this.nachname = nachname;
+    }
+
+    public Student() {
+    }
+}
